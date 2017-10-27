1a8263454937a296b4df3e4a4a6a15add2bbc41f
DonChrapek <daniel.chrapczynski@gmail.com>
2017-10-27 16:28:35 +0200

    Made methods to change password, add alternate email, change mailbox capacity

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -8,7 +8,7 @@ public class Email {
 	private String password;
 	private String department;
 	private String email;
-	private int mailboxCapacity;
+	private int mailboxCapacity = 500;
 	private int defaultPasswordLength = 10;
 	private String alternateEmail;
 	
@@ -50,5 +50,20 @@ public class Email {
 			password[i] = passwordSet.charAt(rand);
 		}
 		return new String(password);
+	}
+	
+	// Set mailbox capacity
+	public void setMailboxCapacity(int capacity) {
+		this.mailboxCapacity = capacity;
+	}
+	
+	// Set alternate email
+	public void setAlternateEmail(String altEmail) {
+		this.alternateEmail = altEmail;
+	}
+	
+	// Change password
+	public void changePassword(String password) {
+		this.password = password;
 	}
 }
